312f4e5e27d3715c90bc10bae551932b437932ea
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-25 14:52:25 +0200

    Create Drink.java

---
+++ MySecondSpringBootApp/src/test/java/com/example/MySecondSpringBootApp/Drink.java
@@ -0,0 +1,32 @@
+package com.example.MySecondSpringBootApp;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+@Slf4j
+
+public class Drink {
+	protected String name;
+	protected String size;
+	protected double calories;
+	protected double price;
+
+	public Drink(String _name, String _size, double _calories, double _price) {
+		this.name = _name;
+		this.size = _size;
+		this.calories = _calories;
+		this.price = _price;
+	}
+
+	@Override
+	public String toString() {
+		return name + ", " + size + ", " + calories + ", " + price;
+	}
+}
