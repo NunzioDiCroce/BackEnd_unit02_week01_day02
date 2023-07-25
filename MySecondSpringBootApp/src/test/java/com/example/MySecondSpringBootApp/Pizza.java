b04646d1dd5ae316b5c269df0982ad294e854f32
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-25 14:49:41 +0200

    Update Pizza.java

--- MySecondSpringBootApp/src/test/java/com/example/MySecondSpringBootApp/Pizza.java
+++ MySecondSpringBootApp/src/test/java/com/example/MySecondSpringBootApp/Pizza.java
@@ -1,5 +1,64 @@
 package com.example.MySecondSpringBootApp;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+@Slf4j
+
 public class Pizza {
+	protected String name;
+	protected String firstTopping;
+	protected String secondTopping;
+	protected String thirdTopping;
+	protected String fourthTopping;
+	protected double calories;
+	protected double price;
+	protected PizzaSize pizzaSize;
+
+	public Pizza(String _name, String _firstTopping, String _secondTopping, double _calories, double _price,
+			PizzaSize _pizzaSize) {
+		this.name = _name;
+		this.firstTopping = _firstTopping;
+		this.secondTopping = _secondTopping;
+		this.calories = _calories;
+		this.price = _price;
+		this.pizzaSize = _pizzaSize;
+	}
+
+	public Pizza(String _name, String _firstTopping, String _secondTopping, String _thirdTopping, double _calories,
+			double _price, PizzaSize _pizzaSize) {
+		this.name = _name;
+		this.firstTopping = _firstTopping;
+		this.secondTopping = _secondTopping;
+		this.thirdTopping = _thirdTopping;
+		this.calories = _calories;
+		this.price = _price;
+		this.pizzaSize = _pizzaSize;
+	}
+
+	public Pizza(String _name, String _firstTopping, String _secondTopping, String _thirdTopping, String _fourthTopping,
+			double _calories, double _price, PizzaSize _pizzaSize) {
+		this.name = _name;
+		this.firstTopping = _firstTopping;
+		this.secondTopping = _secondTopping;
+		this.thirdTopping = _thirdTopping;
+		this.fourthTopping = _fourthTopping;
+		this.calories = _calories;
+		this.price = _price;
+		this.pizzaSize = _pizzaSize;
+	}
+
+	@Override
+	public String toString() {
+		return name + ", " + firstTopping + ", " + secondTopping + ", " + thirdTopping + ", " + fourthTopping + ", "
+				+ calories + ", " + price + ", " + pizzaSize;
+	}
 
 }
