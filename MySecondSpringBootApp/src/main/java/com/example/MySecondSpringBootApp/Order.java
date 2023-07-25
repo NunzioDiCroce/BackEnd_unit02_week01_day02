b3c2db540ff6c32c726a5a5bdee1456b44ae28bd
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-25 16:34:24 +0200

    Update Order.java

--- MySecondSpringBootApp/src/main/java/com/example/MySecondSpringBootApp/Order.java
+++ MySecondSpringBootApp/src/main/java/com/example/MySecondSpringBootApp/Order.java
@@ -1,5 +1,7 @@
 package com.example.MySecondSpringBootApp;
 
+import java.time.LocalDateTime;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,7 +16,11 @@ import lombok.extern.slf4j.Slf4j;
 
 public class Order {
 	protected String number;
-	protected Table table;
+	protected LocalDateTime time;
 	protected OrderStatus orderStatus;
+	protected Table table;
+	protected String seats;
+	// lista elementi menu con variazioni e campo note per ciascun elemento
+	protected double orderPrice; // con coperti come parametro
 
 }
