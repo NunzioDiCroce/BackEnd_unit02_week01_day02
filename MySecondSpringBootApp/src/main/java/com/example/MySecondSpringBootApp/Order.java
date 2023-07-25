abf6ff92730b8bdb8ad30d6534b3b9656b3765c5
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-25 16:53:31 +0200

    Update Order.java

--- MySecondSpringBootApp/src/main/java/com/example/MySecondSpringBootApp/Order.java
+++ MySecondSpringBootApp/src/main/java/com/example/MySecondSpringBootApp/Order.java
@@ -1,6 +1,10 @@
 package com.example.MySecondSpringBootApp;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -20,7 +24,29 @@ public class Order {
 	protected OrderStatus orderStatus;
 	protected Table table;
 	protected String seats;
-	// lista elementi menu con variazioni e campo note per ciascun elemento
+
+	protected List<Pizza> pizzas = new ArrayList<>();
+	protected Map<Pizza, String> pizzaNotes = new HashMap<>();
+
+	protected List<Drink> drinks = new ArrayList<>();
+	protected Map<Drink, String> drinkNotes = new HashMap<>();
+
 	protected double orderPrice; // con coperti come parametro
+
+	public void addPizza(Pizza _pizza) {
+		pizzas.add(_pizza);
+	}
+
+	public void addPizzaNote(Pizza _pizza, String _pizzaNote) {
+		pizzaNotes.put(_pizza, _pizzaNote);
+	}
+
+	public void addDrink(Drink _drink) {
+		drinks.add(_drink);
+	}
+
+	public void addDrinkNote(Drink _drink, String _drinkNote) {
+		drinkNotes.put(_drink, _drinkNote);
+	}
 
 }
